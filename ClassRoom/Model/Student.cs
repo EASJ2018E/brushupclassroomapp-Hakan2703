d1f46790d02af5dc39f4bcfc583110a354a84ab0
Hakan Aslan <haka0089@edu.easj.dk>
2018-09-06 20:07:02 +0200

    Opgave 1 færdig

---
+++ ClassRoom/Model/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom.Model
+{
+    class Student
+    {
+        public Student(string name, int birthmonth, int birthday)
+        {
+            Name = name;
+            Birthmonth = birthmonth;
+            Birthday = birthday;
+        }
+
+        public string Name { get; }
+        public int Birthmonth { get; }
+        public int Birthday { get; }
+    }
+}
