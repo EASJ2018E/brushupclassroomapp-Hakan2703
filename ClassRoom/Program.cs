a8b5639baa924355b07b88254cd9c93a9736f1dc
Hakan Aslan <haka0089@edu.easj.dk>
2018-09-06 21:24:58 +0200

    Opgave 3 done

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassRoom.Model;
 
 namespace ClassRoom
 {
@@ -10,6 +11,19 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+
+           KlasseRum førsteKlasse = new KlasseRum();
+            Student elevNummerEt = new Student("Hakan", 3,27);
+            Student eleverNummerto = new Student("Patrick",1,1);
+
+
+            førsteKlasse.ClassName = "3B";
+            førsteKlasse.SemesterStart = DateTime.Now;
+
+            førsteKlasse.ClassList.Add(elevNummerEt);
+            førsteKlasse.ClassList.Add(eleverNummerto);
+
+
         }
     }
 }
